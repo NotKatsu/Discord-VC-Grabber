911adba45edb2c990bacbe3470bf8a7dd1a47d20
NotKatsu <122122838+NotKatsu@users.noreply.github.com>
2023-07-01 17:32:00 +0100

    Actually return decoded string

--- modules/base64.go
+++ modules/base64.go
@@ -1,6 +1,8 @@
 package modules
 
-import "encoding/base64"
+import (
+	"encoding/base64"
+)
 
 func Encode(text_to_encode string) string {
 	return base64.StdEncoding.EncodeToString([]byte(text_to_encode))
@@ -10,8 +12,8 @@ func Decode(text_to_decode string) string {
 	decoded_string, err := base64.StdEncoding.DecodeString(text_to_decode)
 
 	if err == nil {
-		return text_to_decode
-	} else {
 		return string(decoded_string)
+	} else {
+		return text_to_decode
 	}
 }
