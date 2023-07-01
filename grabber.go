3be2d9cd567dcc7933bbcdd71608ddf59534e88d
NotKatsu <122122838+NotKatsu@users.noreply.github.com>
2023-07-01 16:43:40 +0100

    Encode string

--- grabber.go
+++ grabber.go
@@ -2,11 +2,17 @@ package main
 
 import (
 	"fmt"
+
+	"github.com/NotKatsu/Discord-VC-Grabber/modules"
 )
 
 func main() {
-	var discordUserID int
+	var discordUserID string
 
 	fmt.Printf("Discord User ID: ")
 	fmt.Scanln(&discordUserID)
+
+	result := modules.Encode(string(discordUserID))
+
+	fmt.Println(result)
 }
