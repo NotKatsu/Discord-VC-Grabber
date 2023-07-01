a6b02e2c6133d9b6eb5f20bae06e3b73addd5d16
NotKatsu <122122838+NotKatsu@users.noreply.github.com>
2023-07-01 16:08:20 +0100

    Encode string to base64 and return it

--- modules/base64.go
+++ modules/base64.go
@@ -1,7 +1,9 @@
 package modules
 
-func encode(text_to_encode string) string {
+import "encoding/base64"
 
+func encode(text_to_encode string) string {
+	return base64.StdEncoding.EncodeToString([]byte(text_to_encode))
 }
 
 func decode(text_to_decode string) string {
