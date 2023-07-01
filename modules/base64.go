050eb25650cba5be9a91d4565d00fc8041a56115
NotKatsu <122122838+NotKatsu@users.noreply.github.com>
2023-07-01 16:43:30 +0100

    Make function beginning's capital letters

--- modules/base64.go
+++ modules/base64.go
@@ -2,11 +2,11 @@ package modules
 
 import "encoding/base64"
 
-func encode(text_to_encode string) string {
+func Encode(text_to_encode string) string {
 	return base64.StdEncoding.EncodeToString([]byte(text_to_encode))
 }
 
-func decode(text_to_decode string) string {
+func Decode(text_to_decode string) string {
 	decoded_string, err := base64.StdEncoding.DecodeString(text_to_decode)
 
 	if err == nil {
