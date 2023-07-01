de3d498829ea477b6d4a49cb3e1de040b6c8efb8
NotKatsu <122122838+NotKatsu@users.noreply.github.com>
2023-07-01 16:23:26 +0100

    Simplify code

--- modules/regex.go
+++ modules/regex.go
@@ -5,11 +5,5 @@ import (
 )
 
 func intCheck(number string) bool {
-	var digitCheck = regexp.MustCompile(`^[0-9]+$`)
-
-	if digitCheck.MatchString(number) == true {
-		return true
-	} else {
-		return false
-	}
+	return regexp.MustCompile(`^[0-9]+$`).MatchString(number)
 }
